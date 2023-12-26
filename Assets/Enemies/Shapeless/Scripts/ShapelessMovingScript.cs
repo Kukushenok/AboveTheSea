648cef9da70a45be5e3e720da0469d8418bca10c
IlyaP <mrcoolmoder@gmail.com>
2023-12-26 14:38:43 +0300

    Хочу подключить NavMesh. Поворот, кстати, сделан. Поэтому коммит перед этим)

--- Assets/Enemies/Shapeless/Scripts/ShapelessMovingScript.cs
+++ Assets/Enemies/Shapeless/Scripts/ShapelessMovingScript.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Enemies.Shapeless
 {
+    public class PIDRegulator
+    {
+        public delegate float ErrorFunction(float a, float b);
+        public ErrorFunction errorFunc;
+    }
     public class ShapelessMovingScript : MonoBehaviour
     {
-        [SerializeField] private Vector3 speedDir;
+        [SerializeField] private Transform target;
+        [SerializeField] private Transform directionObject;
+        [SerializeField] private Transform rotationCore;
+        [SerializeField] private float speed;
+        [SerializeField] private float degreesPerSecond;
         [SerializeField] private ShapelessMonsterFeatureAnimator animator;
+        private float rotationAngle = 0;
+        public void Start()
+        {
+            rotationAngle = 0;
+        }
 
         public void Update()
         {
-            transform.Translate(speedDir * animator.speedMultiplier * Time.deltaTime);
+            directionObject.position = rotationCore.position + new Vector3(0, Mathf.Sin(rotationAngle), Mathf.Cos(rotationAngle)) * 3;
+            transform.Translate(directionObject.localPosition.normalized * speed * animator.speedMultiplier * Time.deltaTime);
+            transform.Rotate(new Vector3(0, rotationAngle * Mathf.Deg2Rad, 0) * degreesPerSecond * animator.speedMultiplier * Time.deltaTime);
         }
     }
 }
