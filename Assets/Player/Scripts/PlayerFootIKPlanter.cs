2b64adb316043669373e87e1e6f152bfdb3e7d80
IlyaP <mrcoolmoder@gmail.com>
2024-02-05 22:19:33 +0300

    Я починил ноги. Замечательно. Я хочу попробовать прикрутить звук шагов (поскольку я уже вычисляю когда нога стоит на поверхности когда нет лол)

--- Assets/Player/Scripts/PlayerFootIKPlanter.cs
+++ Assets/Player/Scripts/PlayerFootIKPlanter.cs
@@ -10,21 +10,30 @@ namespace Player
         [SerializeField] private Transform IKReference;
         [SerializeField] private TwoBoneIKConstraint twoBoneIKConstraint;
         [SerializeField] private Transform footTarget;
+        // Shared.
         [SerializeField] private float rayYOffset = 1;
         [SerializeField] private float rayDistance = 0.1f;
         [SerializeField] private float plantedYOffset = 0.1f;
         [SerializeField] private LayerMask mask;
         [SerializeField] private float maxAngleDifference;
+        [Header("Параметры рейкастинга")]
+        [SerializeField, Tooltip("Шаг смещения лучей относительно пятки к носку")] private float forwardOffset = 0.15f;
+        [SerializeField, Tooltip("Кол-во смещений лучей относительно пятки к носку")] private int forwardOffsetCount = 4;
         bool TryPlant(Vector3 localOffset)
         {
-            Vector3 footTargetPos = footTarget.position + footTarget.rotation * localOffset;
+            Vector3 globalOffset = footTarget.rotation * localOffset;
+            globalOffset.y = 0;
+            Vector3 footTargetPos = footTarget.position + globalOffset;
             Vector3 rayPos = Vector3.up * rayYOffset + footTargetPos;
             if (Physics.Raycast(rayPos, Vector3.down, out RaycastHit hit, rayDistance, mask, QueryTriggerInteraction.Ignore))
             {
-                Vector3 plantPos = hit.point + hit.normal * plantedYOffset - footTarget.rotation * localOffset;
+                Vector3 plantLocalPos = hit.point + hit.normal * plantedYOffset;
+                Vector3 plantPos = plantLocalPos - Quaternion.FromToRotation(Vector3.up, hit.normal) * globalOffset;
                 float angleDiff = Vector3.Angle(hit.normal, Vector3.up);
-                Debug.DrawRay(rayPos, Vector3.down * rayDistance, Color.red);
-                if (angleDiff < maxAngleDifference && plantPos.y > footTargetPos.y)
+                
+                Debug.DrawRay(rayPos, Vector3.down * hit.distance, Color.red);
+                Debug.DrawLine(hit.point, plantPos, Color.green);
+                if (angleDiff < maxAngleDifference && plantLocalPos.y > footTargetPos.y)
                 {
                     IKReference.position = plantPos;
                     IKReference.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal) * footTarget.rotation;
@@ -37,12 +46,12 @@ namespace Player
         {
             parentConstraint.weight = 1f;
             float targetWeight = 0f;
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 4; i++)
             {
-                if (TryPlant(Vector3.forward * i / 5.0f))
+                if (TryPlant(Vector3.forward * i * forwardOffset))
                 {
                     targetWeight = 1;
-                    return;
+                    break;
                 }
             }
             //if (Physics.Raycast(footTarget.position + Vector3.up * rayYOffset, Vector3.down, out RaycastHit hit, rayDistance, mask, QueryTriggerInteraction.Ignore))
