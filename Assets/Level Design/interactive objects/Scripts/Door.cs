93d7ba5164d4fc2bcde301b1f11861635b2113cc
svvoooyyyy <79079738+svvoooyyyy@users.noreply.github.com>
2024-05-25 20:19:36 +0300

    level added
    level prototype

--- Assets/Level Design/interactive objects/Scripts/Door.cs
+++ Assets/Level Design/interactive objects/Scripts/Door.cs
@@ -10,7 +10,7 @@ public class Door : MonoBehaviour
     [SerializeField] private string ANIMATION_OPEN_OUT_NAME;
     [SerializeField] private string ANIMATION_CLOSE_IN_NAME;
     [SerializeField] private string ANIMATION_CLOSE_OUT_NAME;
-    [SerializeField] private ButtonObject _buttonObject;
+    [SerializeField] private ButtonObject[] _buttonObjects;
 
     private bool _open = false;
     private enum Direction {none, outd, ind };
@@ -21,7 +21,10 @@ public class Door : MonoBehaviour
 
     private void Awake()
     {
-        _buttonObject.Interacted += OnInteract;
+        foreach (ButtonObject btnobj in _buttonObjects)
+        {
+            btnobj.Interacted += OnInteract;
+        }
         _animator = GetComponent<Animator>();
     }
 
@@ -32,10 +35,10 @@ public class Door : MonoBehaviour
         if(transform.InverseTransformPoint(interactorPosition).x < 0 && _open==false) { direction = Direction.outd; }
         else if (transform.InverseTransformPoint(interactorPosition).x >= 0 && _open == false) { direction = Direction.ind; }
 
-        if (_open==true && direction == Direction.outd) { _animator.Play(ANIMATION_CLOSE_OUT_NAME, 0, 0.0f); _open = false; Debug.Log("close out"); } //todo
-        else if (_open==true && direction == Direction.ind) { _animator.Play(ANIMATION_CLOSE_IN_NAME, 0, 0.0f); _open = false; } //todo
-        else if (_open==false && direction == Direction.outd) { _animator.Play(ANIMATION_OPEN_OUT_NAME, 0, 0.0f); _open = true; Debug.Log("open out"); } //todo
-        else if (_open==false && direction == Direction.ind) { _animator.Play(ANIMATION_OPEN_IN_NAME, 0, 0.0f); _open = true; } //todo
+        if (_open==true && direction == Direction.outd && ANIMATION_CLOSE_OUT_NAME != "") { _animator.Play(ANIMATION_CLOSE_OUT_NAME, 0, 0.0f); _open = false; Debug.Log("close out"); } //todo
+        else if (_open==true && direction == Direction.ind && ANIMATION_CLOSE_IN_NAME != "") { _animator.Play(ANIMATION_CLOSE_IN_NAME, 0, 0.0f); _open = false; } //todo
+        else if (_open==false && direction == Direction.outd && ANIMATION_OPEN_OUT_NAME != "") { _animator.Play(ANIMATION_OPEN_OUT_NAME, 0, 0.0f); _open = true; Debug.Log("open out"); } //todo
+        else if (_open==false && direction == Direction.ind && ANIMATION_OPEN_IN_NAME != "") { _animator.Play(ANIMATION_OPEN_IN_NAME, 0, 0.0f); _open = true; } //todo
     }
     
 }
